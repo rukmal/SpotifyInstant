724c7869033e5ffe160ec140878a0005ab9e4cdf
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2014-04-24 16:29:31 -0700

    Added a Procfile for Heroku deployment

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(express.favicon(__dirname + './public/images/favicon.ico'));
+app.use(express.favicon(__dirname + '/public/images/favicon.ico'));
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
